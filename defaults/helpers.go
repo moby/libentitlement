a2cff89126792b6350f13c543d3d71aa11464891
Nassim 'Nass' Eddequiouaq <eddequiouaq.nassim@gmail.com>
2017-08-28 10:35:23 +0200

    Additional helpers for seccomp profile checks
    Signed-off-by: Nassim 'Nass' Eddequiouaq <eddequiouaq.nassim@gmail.com>

--- defaults/helpers.go
+++ defaults/helpers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 
 	"github.com/docker/libentitlement/secprofile"
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/docker/libentitlement/types"
+	"reflect"
 )
 
 func ociProfileConversionCheck(profile secprofile.Profile, entitlementID string) (*secprofile.OCIProfile, error) {
@@ -17,4 +20,120 @@ func ociProfileConversionCheck(profile secprofile.Profile, entitlementID string)
 	}
 
 	return ociProfile, nil
+}
+
+// FIXME factor for all defaults package and even project-wide
+func testSpec() *specs.Spec {
+	s := &specs.Spec{
+		Process: specs.Process{
+			Capabilities: &specs.LinuxCapabilities{
+				Bounding:    []string{},
+				Effective:   []string{},
+				Inheritable: []string{},
+				Permitted:   []string{},
+				Ambient:     []string{},
+			},
+		},
+		Linux: &specs.Linux{
+			Seccomp:   &specs.LinuxSeccomp{},
+			Resources: &specs.LinuxResources{},
+			IntelRdt:  &specs.LinuxIntelRdt{},
+		},
+		Windows: &specs.Windows{
+			Resources: &specs.WindowsResources{
+				Memory:  &specs.WindowsMemoryResources{},
+				CPU:     &specs.WindowsCPUResources{},
+				Storage: &specs.WindowsStorageResources{},
+				Network: &specs.WindowsNetworkResources{},
+			},
+		},
+	}
+
+	return s
+}
+
+// We can't create a map to check set inclusion with specs.LinuxSeccompArg as key type in Golang so no O(n)
+func seccompArgsContains(syscallArgsSet, syscallArgsSubset []specs.LinuxSeccompArg) bool {
+	if len(syscallArgsSet) < len(syscallArgsSubset) {
+		return false
+	}
+
+	for _, syscallArgFromSubset := range syscallArgsSubset {
+		found := false
+
+		for _, syscallArgsFromSet := range syscallArgsSet {
+			if reflect.DeepEqual(syscallArgFromSubset, syscallArgsFromSet) {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return false
+		}
+	}
+
+	return true
+}
+
+func matchSeccompRule(seccompRule specs.LinuxSyscall, syscallName string, syscallArgs []specs.LinuxSeccompArg) bool {
+	for _, name := range seccompRule.Names {
+		if name == syscallName {
+			if seccompArgsContains(seccompRule.Args, syscallArgs) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func seccompSyscallWithArgsBlocked(seccompProfile specs.LinuxSeccomp, syscallName types.Syscall, syscallArgs []specs.LinuxSeccompArg) bool {
+	syscallNameStr := string(syscallName)
+
+	blocked := seccompProfile.DefaultAction == specs.ActErrno
+
+	if blocked {
+		// For each rule in the seccomp profile, make sure that whitelisting rules don't contain this syscall
+		for _, seccompRule := range seccompProfile.Syscalls {
+			if seccompRule.Action == specs.ActAllow {
+				// If we match a whitelisting rule containing this syscall and those arguments, syscall is not blocked
+				if matchSeccompRule(seccompRule, syscallNameStr, syscallArgs) {
+					return false
+				}
+			}
+		}
+	} else {
+		// For each rule in the seccomp profile, make sure that at least one blacklisting rule contain this syscall
+		for _, seccompRule := range seccompProfile.Syscalls {
+			if seccompRule.Action == specs.ActErrno {
+				// If we match a blacklisting rule containing this syscall and those arguments, syscall is blocked
+				if matchSeccompRule(seccompRule, syscallNameStr, syscallArgs) {
+					return true
+				}
+			}
+		}
+	}
+
+	return blocked
+}
+
+func seccompSyscallsWithArgsBlocked(seccompProfile specs.LinuxSeccomp, syscallsWithArgs map[types.Syscall][]specs.LinuxSeccompArg) bool {
+	for syscallName, syscallArgs := range syscallsWithArgs {
+		if !seccompSyscallWithArgsBlocked(seccompProfile, syscallName, syscallArgs) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func seccompSyscallsBlocked(seccompProfile specs.LinuxSeccomp, syscallNames []types.Syscall) bool {
+	for _, syscallName := range syscallNames {
+		if !seccompSyscallWithArgsBlocked(seccompProfile, syscallName, []specs.LinuxSeccompArg{}) {
+			return false
+		}
+	}
+
+	return true
 }
--- defaults/network-ents_test.go
+++ defaults/network-ents_test.go
@@ -6,110 +6,10 @@ import (
 	"github.com/docker/libentitlement/secprofile"
 	"github.com/stretchr/testify/require"
 	"github.com/docker/libentitlement/types"
+	"syscall"
 )
 
-// FIXME factor for all defaults package and even project-wide
-func testSpec() *specs.Spec {
-	s := &specs.Spec{
-		Process: specs.Process{
-			Capabilities: &specs.LinuxCapabilities{
-				Bounding:    []string{},
-				Effective:   []string{},
-				Inheritable: []string{},
-				Permitted:   []string{},
-				Ambient:     []string{},
-			},
-		},
-		Linux: &specs.Linux{
-			Seccomp:   &specs.LinuxSeccomp{},
-			Resources: &specs.LinuxResources{},
-			IntelRdt:  &specs.LinuxIntelRdt{},
-		},
-		Windows: &specs.Windows{
-			Resources: &specs.WindowsResources{
-				Memory:  &specs.WindowsMemoryResources{},
-				CPU:     &specs.WindowsCPUResources{},
-				Storage: &specs.WindowsStorageResources{},
-				Network: &specs.WindowsNetworkResources{},
-			},
-		},
-	}
 
-	return s
-}
-
-func isEqualSeccompArgs(syscallArgs1, syscallArgs2 []specs.LinuxSeccompArg) bool {
-	if len(syscallArgs1) != len(syscallArgs2) {
-		return false
-	}
-
-	syscallArgMap := make(map[specs.LinuxSeccompArg]bool)
-
-	for _, arg1 := range syscallArgs1 {
-		syscallArgMap[arg1] = true
-	}
-
-	for _, arg2 := range syscallArgs2 {
-		if _, ok := syscallArgMap[arg2]; !ok {
-			return false
-		}
-	}
-
-	return true
-}
-
-func matchSeccompRule(seccompRule specs.LinuxSyscall, syscallName string, syscallArgs []specs.LinuxSeccompArg) bool {
-	for _, name := range seccompRule.Names {
-		if name == syscallName {
-			// FIXME: this function checks equality of arguments but we should check that provided
-			// syscallArgs are a subset of whitelist rule args.
-			if isEqualSeccompArgs(seccompRule.Args, syscallArgs) {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func seccompSyscallWithArgBlocked(seccompProfile specs.LinuxSeccomp, syscallName string, syscallArgs []specs.LinuxSeccompArg) bool {
-	blocked := seccompProfile.DefaultAction == specs.ActErrno
-
-	if blocked {
-		// For each rule in the seccomp profile, make sure that whitelisting rules don't contain this syscall
-		for _, seccompRule := range seccompProfile.Syscalls {
-			if seccompRule.Action == specs.ActAllow {
-				// If we match a whitelisting rule containing this syscall and those arguments, syscall is not blocked
-				if matchSeccompRule(seccompRule, syscallName, syscallArgs) {
-					return false
-				}
-			}
-		}
-	} else {
-		// For each rule in the seccomp profile, make sure that at least one blacklisting rule contain this syscall
-		for _, seccompRule := range seccompProfile.Syscalls {
-			if seccompRule.Action == specs.ActErrno {
-				// If we match a blacklisting rule containing this syscall and those arguments, syscall is blocked
-				if matchSeccompRule(seccompRule, syscallName, syscallArgs) {
-					return true
-				}
-			}
-		}
-	}
-
-	return blocked
-}
-
-func seccompSyscallsBlocked(seccompProfile specs.LinuxSeccomp, syscallNames []types.Syscall) bool {
-	for _, syscallName := range syscallNames {
-		syscallNameStr := string(syscallName)
-		if !seccompSyscallWithArgBlocked(seccompProfile, syscallNameStr, []specs.LinuxSeccompArg{}) {
-			return false
-		}
-	}
-
-	return true
-}
 
 func TestNetworkNoneEntitlementEnforce(t *testing.T) {
 	entitlementID := "network.none"
@@ -139,10 +39,27 @@ func TestNetworkNoneEntitlementEnforce(t *testing.T) {
 		require.Contains(t, newOCIProfile.OCI.Linux.Namespaces, ns)
 	}
 
+	require.NotNil(t, newOCIProfile.OCI.Linux.Seccomp)
+
 	syscallsToBlock := []types.Syscall{SysSocket, SysSocketpair, SysSetsockopt, SysGetsockopt, SysGetsockname, SysGetpeername,
 									   SysBind, SysListen, SysAccept, SysAccept4, SysConnect, SysShutdown, SysRecvfrom, SysRecvmsg, SysRecvmmsg, SysSendto,
 									   SysSendmsg, SysSendmmsg, SysSethostname, SysSetdomainname,
 	}
-	require.NotNil(t, newOCIProfile.OCI.Linux.Seccomp)
 	require.True(t, seccompSyscallsBlocked(*newOCIProfile.OCI.Linux.Seccomp, syscallsToBlock))
+
+	syscallsWithArgsToAllow := map[types.Syscall][]specs.LinuxSeccompArg{
+		SysSocket: {
+			{
+				Index: 0,
+				Op:    specs.OpEqualTo,
+				Value: syscall.AF_UNIX,
+			},
+			{
+				Index: 0,
+				Op:    specs.OpEqualTo,
+				Value: syscall.AF_LOCAL,
+			},
+		},
+	}
+	require.True(t, seccompSyscallsWithArgsBlocked(*newOCIProfile.OCI.Linux.Seccomp, syscallsWithArgsToAllow))
 }
